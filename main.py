322e3e1689929810f764f7f486b97863c91bda94
Vlad Raykov <vladraykov48@gmail.com>
2023-07-25 14:35:39 +0300

    Task2

--- main.py
+++ main.py
@@ -15,6 +15,15 @@ try:
     print(f'Number of digits: {num_count}')
     print(f'Number of letters: {alpha_count}')
 
+    user = input('Enter your data: ')
+
+    user_count = input('Enter data to search: ')
+
+    result1 = len(user.split(user_count))
+    result = result1 - 1
+    print("There are " + str(result) + ' matches')
+
 except ValueError as e:
     print(f'Error: {e}')
     print('Invalid data')
+len(text)])
